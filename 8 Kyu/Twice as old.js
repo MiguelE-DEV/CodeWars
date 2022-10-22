66cd601ea64a63e958078b7a9dcd3a2d74a5ba4f
Miguel Etienne <Etienne.miguel@gmail.com>
2022-10-22 17:40:43 -0400

    First commit

---
+++ 8 Kyu/Twice as old.js
@@ -0,0 +1,7 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    if(dadYearsOld/2 <= sonYearsOld ){ 
+    return (sonYearsOld * 2) - dadYearsOld
+    }else{
+    return dadYearsOld - (sonYearsOld * 2)
+    }
+  }
