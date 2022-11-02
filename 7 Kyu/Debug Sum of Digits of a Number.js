1c283f1e2298463ef64c6b890498ceb4f7e7972f
Miguel Etienne <Etienne.miguel@gmail.com>
2022-11-02 18:45:40 -0400

    soution

---
+++ 7 Kyu/Debug Sum of Digits of a Number.js
@@ -0,0 +1,11 @@
+function getSumOfDigits(integer) {
+    var sum = 0;
+    var digits = String(integer).split('').map(str => Number(str))
+    console.log(digits)
+    for(var ix = 0; ix < digits.length; ix ++) {
+      sum += digits[ix];
+      console.log(digits[ix])
+    }
+    console.log(sum)
+    return sum;
+  }
