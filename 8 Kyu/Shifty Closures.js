cbfe6be77cc253bd8bcf71c3866d7b8393253558
Miguel Etienne <Etienne.miguel@gmail.com>
2022-11-02 18:21:02 -0400

    soution

---
+++ 8 Kyu/Shifty Closures.js
@@ -0,0 +1,8 @@
+var greet_abe = function() {
+    var name = 'Abe';
+    return "Hello, " + name + '!';
+  };
+  var greet_ben = function() {
+    name = 'Ben';
+    return "Hello, " + name + '!';
+  };
