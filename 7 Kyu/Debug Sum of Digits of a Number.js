e51179d557d1dcb7cd953616331a7aecbcbbf9cf
Miguel Etienne <Etienne.miguel@gmail.com>
2023-01-03 21:28:02 -0500

    Solution

--- 7 Kyu/Debug Sum of Digits of a Number.js
+++ 7 Kyu/Debug Sum of Digits of a Number.js
@@ -1,11 +1,13 @@
 function getSumOfDigits(integer) {
-    var sum = 0;
-    var digits = String(integer).split('').map(str => Number(str))
-    console.log(digits)
-    for(var ix = 0; ix < digits.length; ix ++) {
-      sum += digits[ix];
-      console.log(digits[ix])
-    }
-    console.log(sum)
-    return sum;
+  var sum = 0;
+  var digits = String(integer)
+    .split("")
+    .map((str) => Number(str));
+  console.log(digits);
+  for (var ix = 0; ix < digits.length; ix++) {
+    sum += digits[ix];
+    console.log(digits[ix]);
   }
+  console.log(sum);
+  return sum;
+}
---
+++ 8 Kyu/Lost Without a Map.js
@@ -0,0 +1,9 @@
+function maps(x){
+  let y = []
+  for(let i= 0; i<x.length; i++){
+    y.push(x[i]*2)
+  }
+  console.log(x)
+  console.log(y)
+  return y
+}
