c40e3a1867254155f7066442f09dd84765d4dc88
Miguel Etienne <Etienne.miguel@gmail.com>
2023-02-12 23:46:00 -0500

    Practice

---
+++ 8 Kyu/Convert a Number to a String.js
@@ -0,0 +1,5 @@
+function numberToString(num) {
+    let notString = num
+    let isString = notString.toString()
+    return isString
+  }
---
+++ 8 Kyu/Remove First and Last Character.js
@@ -0,0 +1,4 @@
+function removeChar(str){
+    str = str.split('').slice(0,-1).slice(1).join('')
+    return str
+  };
